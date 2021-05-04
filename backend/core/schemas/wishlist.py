acab250c27ad4151dd6343d6a1a94893dfcc5b49
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-04 17:14:19 +0300

    pydantic models moved to core/schemas dir

---
+++ backend/core/schemas/wishlist.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""Pydantic wishlist models."""
+
+import decimal
+import uuid
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class WishlistModel(BaseModel):
+    """Wishlist serializer."""
+
+    name: str
+    slug: str
+    uid: Optional[uuid.UUID] = None
+
+
+class ProductModel(BaseModel):
+    """Product serializer."""
+
+    name: str
+    url: str
+    price: decimal.Decimal
+    uid: Optional[uuid.UUID] = None
+
+
+class ProductModelOut(BaseModel):
+    """Product list serializer."""
+
+    name: str
+    uid: uuid.UUID
+
+    class Config:  # noqa: D106
+        orm_mode = True
+
+
+class ProductWishlistModel(BaseModel):
+    """ProductWishlist serializer."""
+
+    product_uid: uuid.UUID
+    wishlist_uid: uuid.UUID
+    uid: Optional[uuid.UUID] = None
+    reserved: Optional[bool]
+
+
+class ProductWishlistUpdateModel(BaseModel):
+    """ProductWishlist update serializer."""
+
+    product_uid: Optional[uuid.UUID]
+    wishlist_uid: Optional[uuid.UUID]
+    reserved: Optional[bool]
+
+
+class PaginatorModel(BaseModel):
+    """Query paginator serializer."""
+
+    limit: int = 10
+    offset: int = 0
