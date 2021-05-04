af4559df43a3d235601722b6fa759ef040a35726
autoblack <actions@github.com>
2021-05-04 13:13:22 +0000

    fixup: Format Python code with Black

--- backend/tests/snapshots/snap_test_api_wishlist.py
+++ backend/tests/snapshots/snap_test_api_wishlist.py
@@ -7,157 +7,89 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestEmptyWishlist.test_empty_wishlist_full_update 1'] = {
-    'name': 'test-updated',
-    'slug': 'test-slug-updated'
+snapshots["TestEmptyWishlist.test_empty_wishlist_full_update 1"] = {
+    "name": "test-updated",
+    "slug": "test-slug-updated",
 }
 
-snapshots['TestEmptyWishlist.test_empty_wishlist_read 1'] = {
-    'name': 'test',
-    'slug': 'test-slug'
+snapshots["TestEmptyWishlist.test_empty_wishlist_read 1"] = {
+    "name": "test",
+    "slug": "test-slug",
 }
 
-snapshots['TestEmptyWishlist.test_empty_wishlists 1'] = [
-    {
-        'name': 'test1',
-        'slug': 'test-slug1'
-    },
-    {
-        'name': 'test2',
-        'slug': 'test-slug2'
-    },
-    {
-        'name': 'test3',
-        'slug': 'test-slug3'
-    },
-    {
-        'name': 'test4',
-        'slug': 'test-slug4'
-    }
+snapshots["TestEmptyWishlist.test_empty_wishlists 1"] = [
+    {"name": "test1", "slug": "test-slug1"},
+    {"name": "test2", "slug": "test-slug2"},
+    {"name": "test3", "slug": "test-slug3"},
+    {"name": "test4", "slug": "test-slug4"},
 ]
 
-snapshots['TestEmptyWishlist.test_empty_wishlists_paginator_limit 1'] = [
-    {
-        'name': 'test1',
-        'slug': 'test-slug1'
-    },
-    {
-        'name': 'test2',
-        'slug': 'test-slug2'
-    }
+snapshots["TestEmptyWishlist.test_empty_wishlists_paginator_limit 1"] = [
+    {"name": "test1", "slug": "test-slug1"},
+    {"name": "test2", "slug": "test-slug2"},
 ]
 
-snapshots['TestEmptyWishlist.test_empty_wishlists_paginator_offset 1'] = [
-    {
-        'name': 'test3',
-        'slug': 'test-slug3'
-    },
-    {
-        'name': 'test4',
-        'slug': 'test-slug4'
-    }
+snapshots["TestEmptyWishlist.test_empty_wishlists_paginator_offset 1"] = [
+    {"name": "test3", "slug": "test-slug3"},
+    {"name": "test4", "slug": "test-slug4"},
 ]
 
-snapshots['TestEmptyWishlist.test_wishlist_create 1'] = {
-    'name': 'Wishlist1',
-    'slug': 'wishlist1'
+snapshots["TestEmptyWishlist.test_wishlist_create 1"] = {
+    "name": "Wishlist1",
+    "slug": "wishlist1",
 }
 
-snapshots['TestProduct.test_product_create 1'] = {
-    'name': 'Product1',
-    'price': 123.99,
-    'url': 'product1'
+snapshots["TestProduct.test_product_create 1"] = {
+    "name": "Product1",
+    "price": 123.99,
+    "url": "product1",
 }
 
-snapshots['TestProduct.test_product_full_update 1'] = {
-    'name': 'test-updated',
-    'price': 12.0,
-    'url': 'test-url-updated'
+snapshots["TestProduct.test_product_full_update 1"] = {
+    "name": "test-updated",
+    "price": 12.0,
+    "url": "test-url-updated",
 }
 
-snapshots['TestProduct.test_product_paginator_limit 1'] = {
-    'items': [
-        {
-            'name': 'test1'
-        },
-        {
-            'name': 'test2'
-        },
-        {
-            'name': 'test3'
-        },
-        {
-            'name': 'test4'
-        },
-        {
-            'name': 'test5'
-        }
+snapshots["TestProduct.test_product_paginator_limit 1"] = {
+    "items": [
+        {"name": "test1"},
+        {"name": "test2"},
+        {"name": "test3"},
+        {"name": "test4"},
+        {"name": "test5"},
     ],
-    'page': 0,
-    'size': 5,
-    'total': 9
+    "page": 0,
+    "size": 5,
+    "total": 9,
 }
 
-snapshots['TestProduct.test_product_read 1'] = {
-    'name': 'test',
-    'price': 12.3,
-    'url': 'test-url'
+snapshots["TestProduct.test_product_read 1"] = {
+    "name": "test",
+    "price": 12.3,
+    "url": "test-url",
 }
 
-snapshots['TestProduct.test_products 1'] = {
-    'items': [
-        {
-            'name': 'test1'
-        },
-        {
-            'name': 'test2'
-        },
-        {
-            'name': 'test3'
-        },
-        {
-            'name': 'test4'
-        },
-        {
-            'name': 'test5'
-        },
-        {
-            'name': 'test6'
-        },
-        {
-            'name': 'test7'
-        },
-        {
-            'name': 'test8'
-        },
-        {
-            'name': 'test9'
-        }
+snapshots["TestProduct.test_products 1"] = {
+    "items": [
+        {"name": "test1"},
+        {"name": "test2"},
+        {"name": "test3"},
+        {"name": "test4"},
+        {"name": "test5"},
+        {"name": "test6"},
+        {"name": "test7"},
+        {"name": "test8"},
+        {"name": "test9"},
     ],
-    'page': 0,
-    'size': 50,
-    'total': 9
+    "page": 0,
+    "size": 50,
+    "total": 9,
 }
 
-snapshots['TestWishlist.test_offset_wishlist_products_list 1'] = [
-    {
-        'name': 'test6',
-        'price': 126.36,
-        'url': 'test-url6'
-    },
-    {
-        'name': 'test7',
-        'price': 127.37,
-        'url': 'test-url7'
-    },
-    {
-        'name': 'test8',
-        'price': 128.38,
-        'url': 'test-url8'
-    },
-    {
-        'name': 'test9',
-        'price': 129.39,
-        'url': 'test-url9'
-    }
+snapshots["TestWishlist.test_offset_wishlist_products_list 1"] = [
+    {"name": "test6", "price": 126.36, "url": "test-url6"},
+    {"name": "test7", "price": 127.37, "url": "test-url7"},
+    {"name": "test8", "price": 128.38, "url": "test-url8"},
+    {"name": "test9", "price": 129.39, "url": "test-url9"},
 ]
