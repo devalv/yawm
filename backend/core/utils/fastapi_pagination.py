45e0ed5301406a2985077e3844512e36ac27301e
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-07 23:04:25 +0300

    fastapi_pagination Page customization missed params

--- backend/core/utils/fastapi_pagination.py
+++ backend/core/utils/fastapi_pagination.py
@@ -8,12 +8,18 @@ from typing import Generic, Sequence, TypeVar
 from fastapi_pagination import Params
 from fastapi_pagination.bases import AbstractPage, AbstractParams
 
+from pydantic import conint
+
+
 T = TypeVar("T")
 
 
 class JsonApiPage(AbstractPage[T], Generic[T]):
     """JSON:API 1.0 specification says that result key should be a `data`."""
 
+    total: conint(ge=0)  # type: ignore
+    page: conint(ge=0)  # type: ignore
+    size: conint(gt=0)  # type: ignore
     data: Sequence[T]
 
     __params_type__ = Params  # Set params related to Page
@@ -23,4 +29,7 @@ class JsonApiPage(AbstractPage[T], Generic[T]):
         cls, items: Sequence[T], total: int, params: AbstractParams
     ) -> JsonApiPage[T]:
         """Same as the original Page.create instead of `data`."""
-        return cls(data=items)
+        if not isinstance(params, Params):
+            raise ValueError("Page should be used with Params")
+
+        return cls(total=total, data=items, page=params.page, size=params.size)
