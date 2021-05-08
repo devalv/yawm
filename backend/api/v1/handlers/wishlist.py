2ba7724e0d18bf9e1c8c90c8ef11991a235f0b4e
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-09 00:21:11 +0300

    new Wishlist schema

--- backend/api/v1/handlers/wishlist.py
+++ backend/api/v1/handlers/wishlist.py
@@ -9,31 +9,40 @@ from fastapi_pagination.ext.gino import paginate
 
 
 from core.database.models.wishlist import Wishlist  # noqa: I100
-from core.schemas.wishlist import WishlistModel, WishlistModelList  # noqa: I100
+from core.schemas.wishlist import (
+    WishlistCreateModel,
+    WishlistModel,
+    WishlistUpdateModel,
+)
 from core.utils import JsonApiPage  # noqa: I100
 
 wishlist_router = APIRouter(prefix="/api/v1", redirect_slashes=True, tags=["wishlist"])
 
 
-@wishlist_router.get("/wishlists", response_model=JsonApiPage[WishlistModelList])
-async def list_wishlist():  # noqa: B008
+@wishlist_router.get("/wishlists", response_model=JsonApiPage[WishlistModel])
+async def list_wishlist():
     """API for listing all the wishlists."""
-    wishlists = Wishlist.query
-    return await paginate(wishlists)
-
-
-@wishlist_router.post("/wishlists", response_model=WishlistModel)
-async def add_wishlist(wishlist: WishlistModel):
-    """API for creating a new wishlist."""
-    rv = await Wishlist.create(name=wishlist.name)
-    return rv.to_dict()
+    return await paginate(Wishlist.query)
 
 
 @wishlist_router.get("/wishlists/{id}", response_model=WishlistModel)
 async def get_wishlist(id: uuid.UUID):  # noqa: A002
     """API for getting a wishlist."""
-    wishlist = await Wishlist.get_or_404(id)
-    return wishlist.to_dict()
+    return await Wishlist.get_or_404(id)
+
+
+@wishlist_router.post("/wishlists", response_model=WishlistModel)
+async def create_wishlist(wishlist: WishlistCreateModel):
+    """API for creating a new wishlist."""
+    return await Wishlist.create(**wishlist.validated_attributes)
+
+
+@wishlist_router.put("/wishlists/{id}", response_model=WishlistModel)
+async def update_wishlist(id: uuid.UUID, wishlist: WishlistUpdateModel):  # noqa: A002
+    """API for updating a wishlist."""
+    wishlist_obj = await Wishlist.get_or_404(id)
+    await wishlist_obj.update(**wishlist.non_null_attributes).apply()
+    return wishlist_obj
 
 
 @wishlist_router.delete("/wishlists/{id}", response_class=Response, status_code=204)
@@ -41,14 +50,3 @@ async def delete_wishlist(id: uuid.UUID):  # noqa: A002
     """API for deleting a wishlist."""
     wishlist = await Wishlist.get_or_404(id)
     await wishlist.delete()
-
-
-@wishlist_router.put("/wishlists/{id}", response_model=WishlistModel)
-async def update_wishlist(id: uuid.UUID, wishlist: WishlistModel):  # noqa: A002
-    """API for updating a wishlist."""
-    wishlist_obj = await Wishlist.get_or_404(id)
-    # remove empty field
-    wishlist_dict = {k: v for k, v in wishlist.dict().items() if v is not None}
-
-    await wishlist_obj.update(**wishlist_dict).apply()
-    return wishlist_obj.to_dict()
--- backend/core/schemas/wishlist.py
+++ backend/core/schemas/wishlist.py
@@ -1,109 +1,42 @@
 # -*- coding: utf-8 -*-
 """Pydantic wishlist models."""
 
-import uuid
-from typing import Optional
-
 from pydantic import BaseModel
 
+from core.utils import (  # noqa: I100
+    JsonApiPydanticCreateBaseModel,
+    JsonApiPydanticModel,
+    JsonApiPydanticUpdateBaseModel,
+)
 
-class WishlistBaseModel(BaseModel):  # noqa: D101
-    id: Optional[uuid.UUID] = None  # noqa: A002, A003, VNE003
-    type: str  # noqa: A003, VNE003
+
+class WishlistAttributesModel(BaseModel):
+    """Wishlist attributes serializer."""
+
+    name: str
 
 
-class WishlistModel(BaseModel):
+class WishlistUpdateAttributesModel(BaseModel):
+    """Wishlist update attributes serializer."""
+
+    name: str
+
+
+class WishlistModel(JsonApiPydanticModel):
     """Wishlist serializer."""
-
-    name: str
-    id: Optional[uuid.UUID] = None  # noqa: A002, A003, VNE003
-
-
-class WishlistAttributesModel(BaseModel):  # noqa: D101
-    name: str
-
-
-class WishlistModelList(WishlistBaseModel):
-    """Product list serializer."""
 
     attributes: WishlistAttributesModel
 
-    class Config:  # noqa: D106
-        orm_mode = True
+
+class WishlistCreateModel(JsonApiPydanticCreateBaseModel):
+    """Wishlist creation serializer."""
+
+    type: str = "wishlist"  # noqa: A003, VNE003
+    attributes: WishlistAttributesModel
 
 
-class ProductModel(BaseModel):
-    """Product serializer."""
+class WishlistUpdateModel(JsonApiPydanticUpdateBaseModel):
+    """Wishlist update serializer."""
 
-    name: str
-    url: str
-    id: Optional[uuid.UUID] = None  # noqa: A002, A003, VNE003
-
-
-class ProductBaseModel(BaseModel):  # noqa: D101
-    id: Optional[uuid.UUID] = None  # noqa: A002, A003, VNE003
-    type: str  # noqa: A002, A003, VNE003
-
-
-class ProductAttributesModel(BaseModel):  # noqa: D101
-    name: str
-
-
-class ProductModelList(ProductBaseModel):
-    """Product list serializer."""
-
-    attributes: ProductAttributesModel
-
-    class Config:  # noqa: D106
-        orm_mode = True
-
-
-class ProductWishlistModel(BaseModel):
-    """ProductWishlist serializer."""
-
-    wishlist_id: uuid.UUID
-    product_id: uuid.UUID
-    id: Optional[uuid.UUID] = None  # noqa: A002, A003, VNE003
-    substitutable: Optional[bool] = False
-    reserved: Optional[bool] = False
-
-
-class ProductWishlistBaseModel(BaseModel):  # noqa: D101
-    id: Optional[uuid.UUID]  # noqa: A002, A003, VNE003
-    type: str  # noqa: A003, VNE003
-
-
-class ProductWishlistAttributesModel(BaseModel):  # noqa: D101
-    product_id: uuid.UUID
-    wishlist_id: uuid.UUID
-    substitutable: Optional[bool]
-    reserved: Optional[bool]
-
-    class Config:  # noqa: D106
-        orm_mode = True
-
-
-class ProductWishlistModelList(ProductBaseModel):
-    """Product list serializer."""
-
-    attributes: ProductWishlistAttributesModel
-
-    class Config:  # noqa: D106
-        orm_mode = True
-
-
-class AddProductWishlistModel(BaseModel):
-    """ProductWishlist list serializer."""
-
-    product_id: uuid.UUID
-    substitutable: Optional[bool]
-    reserved: Optional[bool]
-
-
-class ProductWishlistUpdateModel(BaseModel):
-    """ProductWishlist update serializer."""
-
-    wishlist_id: Optional[uuid.UUID]
-    product_id: Optional[uuid.UUID]
-    reserved: Optional[bool]
-    substitutable: Optional[bool]
+    type: str = "wishlist"  # noqa: A003, VNE003
+    attributes: WishlistUpdateAttributesModel
