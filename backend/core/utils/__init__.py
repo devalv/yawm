fe440846c2464f578ffa4db1b5174381727c642b
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-08 16:23:08 +0300

    gino extension `JsonApiModel` for JSON:API specification attr `attributes`

--- backend/core/utils/__init__.py
+++ backend/core/utils/__init__.py
@@ -2,5 +2,6 @@
 """Project extra utils."""
 
 from .fastapi_pagination import JsonApiPage
+from .gino_models import JsonApiModel
 
-__all__ = ["JsonApiPage"]
+__all__ = ["JsonApiPage", "JsonApiModel"]
