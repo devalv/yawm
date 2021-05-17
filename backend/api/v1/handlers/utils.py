0894d8e0aa13428a75f7c88d76039a891b309d7b
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-17 23:32:02 +0300

    PageParser tests and pytest-httpx

--- backend/api/v1/handlers/utils.py
+++ backend/api/v1/handlers/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Utils rest-api handlers."""
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 
 from core.schemas import (  # noqa: I100
     ExtractUrlDataInModel,
@@ -12,11 +12,7 @@ from core.services import get_product_name  # noqa: I100
 utils_router = APIRouter(prefix="/api/v1", redirect_slashes=True, tags=["utils"])
 
 
-@utils_router.post(
-    "/extract-product-title",
-    response_model=ExtractUrlModelDataOutModel,
-    status_code=status.HTTP_501_NOT_IMPLEMENTED,
-)
+@utils_router.post("/extract-product-title", response_model=ExtractUrlModelDataOutModel)
 async def extract_name_by_url(product_url: ExtractUrlDataInModel):
     """Extract product name by url."""
     # TODO: change status code
