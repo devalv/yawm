2c1d34a8f7c62eee298fb4ee4238bf813ea54531
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-19 20:28:40 +0300

    pagination page links

--- backend/core/utils/fastapi_pagination.py
+++ backend/core/utils/fastapi_pagination.py
@@ -3,12 +3,15 @@
 
 from __future__ import annotations
 
-from typing import Generic, Sequence, TypeVar
+from math import ceil
+from typing import Any, Generic, Sequence, TypeVar
 
 from fastapi_pagination import Params
 from fastapi_pagination.bases import AbstractPage, AbstractParams
+from fastapi_pagination.links.bases import Links, create_links
 
 from pydantic import conint
+from pydantic import root_validator
 
 
 T = TypeVar("T")
@@ -21,6 +24,7 @@ class JsonApiPage(AbstractPage[T], Generic[T]):
     page: conint(ge=0)  # type: ignore
     size: conint(gt=0)  # type: ignore
     data: Sequence[T]
+    links: Links
 
     __params_type__ = Params  # Set params related to Page
 
@@ -33,3 +37,18 @@ class JsonApiPage(AbstractPage[T], Generic[T]):
             raise ValueError("Page should be used with Params")
 
         return cls(total=total, data=items, page=params.page, size=params.size)
+
+    @root_validator(pre=True)
+    def __root_validator__(cls, value: Any) -> Any:
+        """Pagination links builder."""
+        if "links" not in value:
+            page, size, total = [value[k] for k in ("page", "size", "total")]
+
+            value["links"] = create_links(
+                first={"page": 0},
+                last={"page": ceil(total - 1 / size)},
+                next={"page": page + 1} if (page + 1) * size < total else None,
+                prev={"page": page - 1} if 0 <= page - 1 else None,
+            )
+
+        return value
