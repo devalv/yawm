9e137613ce72c6f69e5b6fc9fad46cc057085ab9
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-05-19 19:46:51 +0300

    search existing product name by url

--- backend/api/v1/handlers/utils.py
+++ backend/api/v1/handlers/utils.py
@@ -8,6 +8,7 @@ from core.schemas import (  # noqa: I100
     ExtractUrlModelDataOutModel,
 )
 from core.services import get_product_name  # noqa: I100
+from core.database import ProductGinoModel  # noqa: I100
 
 utils_router = APIRouter(prefix="/api/v1", redirect_slashes=True, tags=["utils"])
 
@@ -15,6 +16,12 @@ utils_router = APIRouter(prefix="/api/v1", redirect_slashes=True, tags=["utils"]
 @utils_router.post("/extract-product-title", response_model=ExtractUrlModelDataOutModel)
 async def extract_name_by_url(product_url: ExtractUrlDataInModel):
     """Extract product name by url."""
-    # TODO: change status code
-    value = await get_product_name(product_url.data.attributes.url)
+    url = product_url.data.attributes.url
+    product_with_url = await ProductGinoModel.query.where(
+        ProductGinoModel.url == url
+    ).gino.first()
+
+    if product_with_url:
+        return {"data": {"attributes": {"h1": product_with_url.name}}}
+    value = await get_product_name(url)
     return {"data": {"attributes": {"h1": value}}}
