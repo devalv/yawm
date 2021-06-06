438e575d48166e322119a56c2428cc4d89e25f45
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-06-06 19:58:43 +0300

    WIP: user schema notes

--- backend/core/schemas/security.py
+++ backend/core/schemas/security.py
@@ -19,3 +19,14 @@ class User(BaseModel):
     email: str = None
     full_name: str = None
     disabled: bool = None
+    family_name: str = None
+    given_name: str = None
+    picture: str = None
+
+    # sub - Google user id
+    # print('sub:', credentials.id_token['sub'])
+    # print('email:', credentials.id_token['email'])
+    # print('name:', credentials.id_token.get('name', None))
+    # print('picture:', credentials.id_token.get('picture', None))
+    # print('given_name:', credentials.id_token.get('given_name', None))
+    # print('family_name:', credentials.id_token.get('family_name', None))
