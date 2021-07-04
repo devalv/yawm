19233b25a883b350ded7eaa7f8b2e187cdfe3bfb
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-07-04 19:03:15 +0300

    TokenInfo db table.

--- backend/core/database/migrations/versions/85b9bef9bbee_initial_migration.py
+++ backend/core/database/migrations/versions/85b9bef9bbee_initial_migration.py
@@ -20,6 +20,14 @@ depends_on = None
 def upgrade():
     """Apply changes on database."""
     op.create_table(
+        "product",
+        sa.Column("id", postgresql.UUID(), nullable=False),
+        sa.Column("name", sa.Unicode(length=255), nullable=False),
+        sa.Column("url", sa.Unicode(length=8000), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("url"),
+    )
+    op.create_table(
         "user",
         sa.Column("id", postgresql.UUID(), nullable=False),
         sa.Column("ext_id", sa.Unicode(length=255), nullable=False),
@@ -29,7 +37,7 @@ def upgrade():
             "created",
             sa.DateTime(timezone=True),
             server_default=sa.text("now()"),
-            nullable=True,
+            nullable=False,
         ),
         sa.Column("username", sa.Unicode(length=255), nullable=False),
         sa.Column("given_name", sa.Unicode(length=255), nullable=True),
@@ -40,18 +48,29 @@ def upgrade():
     )
     op.create_index(op.f("ix_user_username"), "user", ["username"], unique=False)
     op.create_table(
-        "product",
-        sa.Column("id", postgresql.UUID(), nullable=False),
-        sa.Column("name", sa.Unicode(length=255), nullable=False),
-        sa.Column("url", sa.Unicode(length=8000), nullable=False),
-        sa.PrimaryKeyConstraint("id"),
-        sa.UniqueConstraint("url"),
-    )
-    op.create_table(
         "wishlist",
         sa.Column("id", postgresql.UUID(), nullable=False),
         sa.Column("name", sa.Unicode(length=255), nullable=False),
         sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "token_info",
+        sa.Column("user_id", postgresql.UUID(), nullable=False),
+        sa.Column("refresh_token", sa.Unicode(), nullable=False),
+        sa.Column(
+            "created",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.ForeignKeyConstraint(["user_id"], ["user.id"], ondelete="CASCADE"),
+        sa.PrimaryKeyConstraint("user_id"),
+    )
+    op.create_index(
+        op.f("ix_token_info_refresh_token"),
+        "token_info",
+        ["refresh_token"],
+        unique=False,
     )
     op.create_table(
         "wishlist_products",
@@ -68,8 +87,10 @@ def upgrade():
 
 def downgrade():
     """Revert changes on database."""
+    op.drop_table("wishlist_products")
+    op.drop_index(op.f("ix_token_info_refresh_token"), table_name="token_info")
+    op.drop_table("token_info")
+    op.drop_table("wishlist")
     op.drop_index(op.f("ix_user_username"), table_name="user")
     op.drop_table("user")
-    op.drop_table("wishlist_products")
-    op.drop_table("wishlist")
     op.drop_table("product")
