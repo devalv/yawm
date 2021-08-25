45a64cc5e1e8b199a75fcfbc08929969e9c53cb1
Aleksei Devyatkin <aleksei@devyatkin.dev>
2021-08-26 01:01:21 +0300

    fastapi-0.3.0: ready4frontend pt. 1.5
    * linter fixes

--- backend/core/schemas/security/__init__.py
+++ backend/core/schemas/security/__init__.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """Pydantic security models."""
 
-# from .auth import UserDataCreateModel, UserDataUpdateModel, UserDBDataModel, UserDBModel
 from .auth import BaseUserModel, UserCreateModel, UserViewModel
 from .oauth2 import AccessToken, GoogleIdInfo, RefreshToken, Token
 
 __all__ = [
-    # "UserDBModel",
-    # "UserDBDataModel",
-    # "UserDataCreateModel",
-    # "UserDataUpdateModel",
+    "BaseUserModel",
+    "UserCreateModel",
+    "UserViewModel",
     "Token",
     "AccessToken",
     "RefreshToken",
