9bcfcbfa6b2ef661f2108e6a4cda59bbecaae6b9
Aleksei Devyatkin <aleksei@devyatkin.dev>
2022-04-08 15:51:26 +0300

    Uvicorn worker configuration

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,14 @@
 """Simple debug application runner."""
 
 import uvicorn
+from uvicorn.workers import UvicornWorker
 
 from core import config
+
+
+class GunicornUvicornWorker(UvicornWorker):
+    CONFIG_KWARGS = {"loop": "uvloop", "http": "auto"}
+
 
 if __name__ == "__main__":
     uvicorn.run(
